7b11ad575fe036505d0f346b39ddf72d30e875aa
BonaFideDaniel <BonafideDaniel@gmail.com>
2025-02-04 14:26:44 -0500

    yeoman generator

---
+++ src/webparts/standardWorkPackage/components/IStandardWorkPackageProps.ts
@@ -0,0 +1,7 @@
+export interface IStandardWorkPackageProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/standardWorkPackage/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IStandardWorkPackageWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'StandardWorkPackageWebPartStrings' {
+  const strings: IStandardWorkPackageWebPartStrings;
+  export = strings;
+}
